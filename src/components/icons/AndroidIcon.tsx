9e2017080b18ae9af0b442eea40ab68802a605b9
Ekka-Barber <ekka.barber@gmail.com>
2025-04-04 04:50:39 +0300

    Update AndroidIcon.tsx

--- src/components/icons/AndroidIcon.tsx
+++ src/components/icons/AndroidIcon.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 
-const AndroidIcon = () => (
+const AndroidIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     height="32px" 
     viewBox="0 -960 960 960" 
     width="32px" 
     fill="currentColor"
-    className="shrink-0 animate-[heart-beat_2s_cubic-bezier(0.4,0,0.6,1)_infinite] hardware-accelerated"
+    className="shrink-0 animate-heart-beat transform-gpu"
     style={{ transform: 'translateZ(0)' }}
+    {...props}
   >
     <path d="M40-240q9-107 65.5-197T256-580l-74-128q-6-9-3-19t13-15q8-5 18-2t16 12l74 128q86-36 180-36t180 36l74-128q6-9 16-12t18 2q10 5 13 15t-3 19l-74 128q94 53 150.5 143T920-240H40Zm240-110q21 0 35.5-14.5T330-400q0-21-14.5-35.5T280-450q-21 0-35.5 14.5T230-400q0 21 14.5 35.5T280-350Zm400 0q21 0 35.5-14.5T730-400q0-21-14.5-35.5T680-450q-21 0-35.5 14.5T630-400q0 21 14.5 35.5T680-350Z"/>
   </svg>
