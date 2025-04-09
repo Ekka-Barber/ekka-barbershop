eecf5574fbc42529de42588469cc0241f2931d15
Ekka-Barber <ekka.barber@gmail.com>
2025-04-09 19:03:21 +0300

    Add files via upload

---
+++ src/lib/salary/index.ts
@@ -0,0 +1,12 @@
+// Export types
+export * from './types/salary';
+
+// Export calculators
+export * from './calculators/BaseCalculator';
+export * from './calculators/CalculatorFactory';
+export * from './calculators/FixedCalculator';
+export * from './calculators/CommissionCalculator';
+export * from './calculators/TieredCommissionCalculator';
+
+// Export hooks
+export * from './hooks/useSalaryCalculation'; 
