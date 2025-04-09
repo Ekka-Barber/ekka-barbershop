9eea025e4688e2cd3709e52caa0855608b819e4c
Ekka-Barber <ekka.barber@gmail.com>
2025-04-09 19:04:57 +0300

    Update employee.ts

--- src/types/employee.ts
+++ src/types/employee.ts
@@ -17,6 +17,7 @@ export interface Employee {
   working_hours: WorkingHours;
   off_days?: string[];
   nationality?: string;
+  salary_plan_id?: string;
   created_at?: string;
   updated_at?: string;
 }
