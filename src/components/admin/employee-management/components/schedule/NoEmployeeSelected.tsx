3bbe609e7062ebc17ce1f8115fad5b839d69f0e1
SafwatalJood <almalkyf255@gmail.com>
2025-05-12 20:03:03 +0300

    whole update - removing all employee management componants

--- src/components/admin/employee-management/components/schedule/NoEmployeeSelected.tsx
+++
@@ -1,8 +0,0 @@
-
-export const NoEmployeeSelected = () => {
-  return (
-    <div className="flex items-center justify-center p-8 bg-muted/30 rounded-lg">
-      <p className="text-muted-foreground">Select an employee to manage their schedule</p>
-    </div>
-  );
-};
