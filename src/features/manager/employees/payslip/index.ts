c718ac83702e04ec7c912629534311bc9fde11aa
SafwatalJood <almalkyf255@gmail.com>
2026-01-28 16:16:03 +0300

    phase 4 DONE

--- src/features/manager/employees/payslip/index.ts
+++
@@ -1,8 +0,0 @@
-// Barrel exports for payslip components
-
-export { LazyPDFViewer } from './LazyPDFWrapper';
-export { default as PayslipButton } from './PayslipButton';
-export { PayslipModal } from './PayslipModal';
-export { default as PayslipViewer } from './PayslipViewer';
-export * from './formatters';
-export { PAYSLIP_COLORS, SALES_MILESTONE_COLORS } from './styleConstants';
--- src/features/manager/employees/salary/index.ts
+++
@@ -1,9 +0,0 @@
-// Barrel exports for salary components
-
-export { BonusDetails } from './BonusDetails';
-export { BonusList } from './BonusList';
-export { DeductionDetails } from './DeductionDetails';
-export { LoanDetails } from './LoanDetails';
-export { SalaryExplanation } from './SalaryExplanation';
-export { SalaryNetBlock } from './SalaryNetBlock';
-export { SalaryRow } from './SalaryRow';
